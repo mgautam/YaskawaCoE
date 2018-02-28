b69c632077077dd0e7b4bf2a6da296e2a66a7236
GautamManoharan <gautam.raw@gmail.com>
2018-02-28 16:42:53 +1100

    ppm tested

--- build.sh
+++ build.sh
@@ -4,15 +4,3 @@ mkdir build
 cd build
 cmake ..
 make -j8
-
-pyenv local 3.5.1
-virtualenv -p python3.5 guivenv
-cd guivenv
-. bin/activate
-#cp ../../gui/* .
-ln -s ../../gui/control.kv control.kv
-ln -s ../../gui/requirements.txt requirements.txt
-ln -s ../../gui/ycoe_gui.py ycoe_gui.py
-pip install -r requirements.txt
-cd ../..
-
