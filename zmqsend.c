5436efaa3b6ae17bdf2bb4e8eed7fda4ec7ca730
GautamManoharan <gautam.raw@gmail.com>
2018-12-06 16:14:40 +1100

    zmqsend profiles combos

--- zmqsend.c
+++ zmqsend.c
@@ -2,6 +2,7 @@
 #include "ycoe_math.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <zmq.h>
 
 #define MAX_POSARR_LEN    2500
@@ -16,19 +17,49 @@ int main(int argc, char *argv[]) {
     trifill(_tri_pos_arr, 0, 6400000.0, MAX_POSARR_LEN);//6400000=100000counts/s
     trifill(_tri_pos_arr+MAX_POSARR_LEN, 0, 6400000.0, MAX_POSARR_LEN);// 800000=12500counts/s
 
+    DINT *_pos_arr = malloc(2*MAX_POSARR_LEN*sizeof(DINT));
+
     char buffer[15] = {0};
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
     zmq_connect (requester, "tcp://localhost:5555");
     while (1)
     {
-      zmq_send(requester, (char *)_sin_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+      memset(_pos_arr,0, MAX_POSARR_LEN*sizeof(DINT));
+      memcpy((_pos_arr+MAX_POSARR_LEN),_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
       zmq_recv(requester,buffer,12,0);
       sleep(3);// Sleep 3 seconds
-      zmq_send(requester, (char *)_tri_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+
+      memcpy(_pos_arr,_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      memset((_pos_arr+MAX_POSARR_LEN),0, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
       zmq_recv(requester,buffer,12,0);
       sleep(3);// Sleep 3 seconds*/
-      //osal_usleep(300000);
+
+      memcpy(_pos_arr,_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      memcpy((_pos_arr+MAX_POSARR_LEN),_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+      zmq_recv(requester,buffer,12,0);
+      sleep(3);// Sleep 3 seconds
+
+      memcpy(_pos_arr,_sin_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      memcpy((_pos_arr+MAX_POSARR_LEN),_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+      zmq_recv(requester,buffer,12,0);
+      sleep(3);// Sleep 3 seconds
+
+      memcpy(_pos_arr,_tri_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      memcpy((_pos_arr+MAX_POSARR_LEN),_sin_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+      zmq_recv(requester,buffer,12,0);
+      sleep(3);// Sleep 3 seconds*/
+      
+      memcpy(_pos_arr,_sin_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      memcpy((_pos_arr+MAX_POSARR_LEN),_sin_pos_arr, MAX_POSARR_LEN*sizeof(DINT));
+      zmq_send(requester, (char *)_pos_arr, 2*MAX_POSARR_LEN*sizeof(DINT), 0);
+      zmq_recv(requester,buffer,12,0);
+      sleep(3);// Sleep 3 seconds
     }
     free(_sin_pos_arr);
     free(_tri_pos_arr);
